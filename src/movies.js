895fe9794da439a14f1567c90a4b9a9d0a839eec
Carolina Braga <brgacarolina@gmail.com>
2023-04-28 20:15:39 -0400

    solve 2 iterations

--- src/movies.js
+++ src/movies.js
@@ -66,10 +66,33 @@ function dramaMoviesScore(moviesArray) {
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+
+  const onlyYears = moviesArray.filter((array) => {
+    return array.year;
+    });
+    
+  const moviesByYear = onlyYears.sort((a, b) => {
+
+    return a.year - b.year; 
+  });
+
+  return moviesByYear;
+
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+
+const movieTitles = moviesArray.map((array) => {
+return array.title;
+})
+
+const ordered = movieTitles.sort();
+
+return ordered.slice(0,20)
+
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
